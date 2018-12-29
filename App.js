999f63ed82aee835a8a811c76a63bcb00ed30165
Apostol <nikov.apostol@gmail.com>
2018-12-29 19:27:39 +0200

    added: eslint and firebase sdk

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
 import styled from 'styled-components';
+import * as firebase from 'firebase';
+import { firebaseConfig } from './src/config/firebaseConfig';
+
+firebase.initializeApp(firebaseConfig);
+
+// test connection
+// firebase.database().ref('users/').child('d1qsqd1wqsad1').on('value', (snapshot) => {
+//   console.log(snapshot.val());
+// });
 
 export default class App extends React.Component {
   render() {
